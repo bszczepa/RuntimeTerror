d488de900efbb1748aef44500d08dd833134c264
Mateusz Sutor <msutor@student.agh.edu.pl>
2020-06-21 00:54:16 +0200

    Read comment in App.java

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -11,7 +11,13 @@ public class App {
     public static void main(String[] args) throws IOException, InvalidFormatException {
         UserControl userControl = new UserControl();
         userControl.controlLoop();
-    	
+
+        /**
+         * w UserControl musicie sobie zmienić ścieżke do folderu z danymi zeby ruszylo
+         * na sztywno wpisalem gdyby klient chciał zobaczyć przykladowy raport
+         * działa 1 i 5
+         */
+
 //    	Model model = new Model("TU WPISAC SCIEZKE DO PLIKOW", null);    // drugim parametrem jest nazwisko_imie
 //    																	//  jeżeli jest null to znaczy ze przeglądamy wszystkie pliki
 //    	
