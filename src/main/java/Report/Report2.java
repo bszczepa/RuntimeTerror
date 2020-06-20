02b708c1d0c364ab3864882a9c4ed2c852183302
martatomalska <tomalska.marta@gmail.com>
2020-06-21 01:46:15 +0200

    add Report2

---
+++ src/main/java/Report/Report2.java
@@ -0,0 +1,37 @@
+package Report;
+
+import Model.*;
+import java.util.*;
+
+public class Report2 {
+
+    public void createReport2(Model model, int year) {
+
+        TreeMap<String, Double> projectsMap = new TreeMap<>();
+        List<Employee> employees = model.getEmployeeList();
+
+        for (Employee employee : employees) {
+            for (Task task : employee.getTaskList()) {
+                Date date = task.getTaskDate();
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(date);
+                if (calendar.get(Calendar.YEAR) == year) {
+                    String projectName = task.getProjectName();
+                    if (projectsMap.containsKey(projectName)) {
+                        projectsMap.replace(projectName, projectsMap.get(projectName) + task.getHours());
+                    } else {
+                        projectsMap.put(projectName, task.getHours());
+                    }
+                }
+            }
+        }
+
+        System.out.println("Raport 2 - lista projektów\n");
+        for (Map.Entry project : projectsMap.entrySet()) {
+            System.out.println(project.getKey() + "    " + project.getValue());
+        }
+    }
+
+
+
+}
