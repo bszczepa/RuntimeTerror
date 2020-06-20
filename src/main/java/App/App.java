baf09771b7a257347ab3fab0f8b5263ac52a8969
MateuszSutor <mateusz.sutor@icloud.com>
2020-06-20 13:24:27 +0200

    Merge branch 'master' of C:\Users\Mateusz\IdeaProjects\agh\RuntimeTerror with conflicts.

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -1,4 +1,12 @@
 package App;
 
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Start");
+
+        UserControl userControl = new UserControl();
+
+        userControl.showOption();
+    }
+
 }
