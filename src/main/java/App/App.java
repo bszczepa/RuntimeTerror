f15e239be597e1dbb43a32ff269929b96f0b8bbd
Anna Madej <aniamadej123@gmail.com>
2020-06-20 20:03:56 +0200

    cleared app class

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -2,23 +2,11 @@ package App;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
-import Model.Model;
-import Report.Report5;
-
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException, InvalidFormatException {
-//        UserControl userControl = new UserControl();
-//        userControl.controlLoop();
-    	
-    	
-    	
-    	Model model = new Model("D:/Users/aniam/Desktop/TERROR/RuntimeTerror/src/Resources/reporter-dane", null);
-    	
-    	Report5 report5 = new Report5();
-    	report5.createReport(model.getEmployeeList());
-    	
-    	report5.printReport();
+        UserControl userControl = new UserControl();
+        userControl.controlLoop();
     }
 }
