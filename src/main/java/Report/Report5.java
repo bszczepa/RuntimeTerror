bea7f8c2dc0b07e0435dd580ba81ebc00ce6c7e3
Anna Madej <aniamadej123@gmail.com>
2020-06-20 21:24:35 +0200

    changed report 5

--- src/main/java/Report/Report5.java
+++ src/main/java/Report/Report5.java
@@ -6,24 +6,32 @@ import java.util.List;
 import com.sun.rowset.internal.Row;
 
 import Model.Employee;
+import Model.Model;
 import Model.Task;
 
 public class Report5 {
 
+	private Integer rowsCounter = 1;
 	private List<String> columnNames = new ArrayList<String>();
 	private List<List<String>> rows = new ArrayList<List<String>>();
 
-	public void createReport(List<Employee> employees) {
+	
+	
+	public Report5(Model model) {
+
+		columnNames.add("L.p");
 		columnNames.add("Imię i nazwisko");
 		columnNames.add("Projekt");
 		columnNames.add("Ilość godzin");
+
+		List<Employee> employees = model.getEmployeeList();
 
 		for (Employee employee : employees) {
 			for (Task task : employee.getTaskList()) {
 				Integer indexOfRowToChange = null;
 				for (List<String> row : rows) {
-					String employeeInRow = row.get(0);
-					String projectInRow = row.get(1);
+					String employeeInRow = row.get(1);
+					String projectInRow = row.get(2);
 					
 					if (employeeInRow.equals(employee.getNameAndSurname()) && projectInRow.equals(task.getProjectName())) {
 						indexOfRowToChange = rows.indexOf(row);
@@ -32,19 +40,23 @@ public class Report5 {
 
 				if (indexOfRowToChange != null) {
 					List<String> rowToChange = rows.get(indexOfRowToChange);
-					Double hoursToChange  = Double.valueOf(rowToChange.get(2));
+					Double hoursToChange  = Double.valueOf(rowToChange.get(3));
 					Double newHours = hoursToChange + task.getHours();
-					rowToChange.set(2, newHours.toString());
+					rowToChange.set(3, newHours.toString());
 				} else {
 					List<String> newRow = new ArrayList<String>();
+					newRow.add(rowsCounter.toString());
 					newRow.add(employee.getNameAndSurname());
 					newRow.add(task.getProjectName());
 					newRow.add(task.getHours().toString());
 					rows.add(newRow);
+					rowsCounter++;
 				}
 
 			}
 		}
+		
+		
 	}
 
 	public void printReport() {
