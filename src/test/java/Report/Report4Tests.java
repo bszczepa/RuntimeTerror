d5d1f075552f29d0099a1d8da6713cf950867c45
Anna Madej <aniamadej123@gmail.com>
2020-06-23 14:51:00 +0200

    added testNotCountingPercentsFromDifferentYears to report4Tests

--- src/test/java/Report/Report4Tests.java
+++ src/test/java/Report/Report4Tests.java
@@ -31,18 +31,15 @@ public class Report4Tests {
 		Task task3 = new Task(date, "jakisProjekt3", "jakies zadanie2", 7);
 		employee2.addTask(task2);
 		employee2.addTask(task3);
-		
-		
-		
+
 		employees.add(employee1);
 		employees.add(employee2);
-		
-		
+
 		Model model = Mockito.mock(Model.class);
 		Mockito.when(model.getEmployeeList()).thenReturn(employees);
-		
+
 		ReportBuilder rBuilder = new Report4Builder(2020);
-		
+
 		Report report = rBuilder.buildReport(model);
 		Assert.assertEquals(4, report.getColumnNames().size());
 		Assert.assertEquals(2, report.getRows().size());
@@ -51,19 +48,17 @@ public class Report4Tests {
 		Assert.assertEquals("50.0%", report.getRows().get(1).get(2));
 		Assert.assertEquals("50.0%", report.getRows().get(1).get(3));
 
+
 	}
-	
+
 	@Test
 	public void testNotFilteringMasterEmployeesData() throws IOException {
 
-	
-		
-		
 		List<Employee> employees = new ArrayList<Employee>();
-		
+
 		Model model = Mockito.mock(Model.class);
 		Mockito.when(model.getEmployeeList()).thenReturn(employees);
-		
+
 		Employee employee1 = new Employee("Jan", "Nowak");
 		Calendar myCalendar = new GregorianCalendar(2012, 2, 11);
 		Date date = myCalendar.getTime();
@@ -74,12 +69,43 @@ public class Report4Tests {
 		rBuilder.buildReport(model);
 		ReportBuilder rBuilder2 = new Report4Builder(2012);
 		Report report = rBuilder2.buildReport(model);
-		ReportPrinter.printReport(report);
-		
-		
+
 		Assert.assertTrue((report.getRows().size() == 1));
+	}
+	
+	@Test
+	public void testNotCountingPercentsFromDifferentYears() throws IOException {
+
+		List<Employee> employees = new ArrayList<Employee>();
 		
-		ReportPrinter.printReport(report);
+		Model model = Mockito.mock(Model.class);
+		Mockito.when(model.getEmployeeList()).thenReturn(employees);
+		
+		Employee employee1 = new Employee("Jan", "Nowak");
+		Calendar myCalendar = new GregorianCalendar(2012, 2, 11);
+		Date date = myCalendar.getTime();
+		Task task = new Task(date, "jakisProjekt", "jakies zadanie", 3);
+		Task task1 = new Task(date, "innyProjekt", "jakies zadanie", 3);
+		
+		myCalendar = new GregorianCalendar(2013, 2, 11);
+		date = myCalendar.getTime();
+		Task task2 = new Task(date, "innyProjekt", "jakies zadanie", 3);
+		employee1.addTask(task);
+		employee1.addTask(task1);
+		employee1.addTask(task2);
+		employees.add(employee1);
+		ReportBuilder rBuilder = new Report4Builder(2012);
+		Report report = rBuilder.buildReport(model);
+		
+		
+		
+		Assert.assertEquals(4, report.getColumnNames().size());
+		Assert.assertEquals(1, report.getRows().size());
+		Assert.assertEquals("50.0%", report.getRows().get(0).get(2));
+		Assert.assertEquals("50.0%", report.getRows().get(0).get(3));
+
+		
+
 
 	}
 
